335750a1149fe2953c4517fcc7907f1a65b0a6e2
Kyle Corbin <kylecorbin26@gmail.com>
2019-10-29 19:04:17 -0400

    Setting up nested get requests

--- github/src/App.js
+++ github/src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
 import './App.css';
 import GitList from "./components/GitList";
+import GitUser from "./components/GitUser";
 import axios from "axios";
 
 class App extends React.Component {
   
   
     state={
+      host:{},
+      followers:'',
       data:[]
     }
   
 
   componentDidMount(){
     axios
-      .get("https://api.github.com/users/YeetOrBeYate/followers")
+      .get("https://api.github.com/users/YeetOrBeYate")
       .then((res)=>{
+        const host = res.data;
         this.setState({
-          data: res.data
+          host: {
+            image:host.avatar_url,
+            login:host.login,
+            location:host.location
+          },
+          followers:host.followers_url
         })
         console.log("App",res.data)
+        axios
+        .get(`${this.state.followers}`)
+        .then((res)=>{
+          console.log("this is followers", res)
+        })
       })
       .catch((err)=>{
         console.log(err)
@@ -30,7 +44,11 @@ class App extends React.Component {
     return (
       <div className="App">
         Yeet
-        <GitList data={this.state.data}/>
+        <GitUser 
+        login={this.state.host.login}
+        image={this.state.host.image}
+        location={this.state.host.location}/>
+        {/* <GitList data={this.state.data}/> */}
       </div>
     );
   }
--- github/src/components/GitUser.js
+++ github/src/components/GitUser.js
@@ -7,7 +7,9 @@ class GitUser extends React.Component{
     render(){
         return(
             <div>
-                
+                <h1>{this.props.login}</h1>
+                <img src={this.props.image}/>
+                <p>{this.props.location}</p>
             </div>
         );
     }
